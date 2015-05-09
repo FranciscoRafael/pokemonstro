6baa5a46650ff5c2671130507a9088968cf9e34c
Danilo Charântola <danilocharantola@hotmail.com>
2015-05-09 17:03:49 -0300

    ajuste BD.

--- pokemonstro/src/backup.sql
+++ pokemonstro/src/backup.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.5.27-log - MySQL Community Server (GPL)
 -- Server OS:                    Win32
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2015-05-09 16:58:01
+-- Date/time:                    2015-05-09 17:03:02
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
